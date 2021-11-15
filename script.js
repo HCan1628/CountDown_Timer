ea81d7622ea3788e36e3ab29c4ca53787d9379e6
fearm3 <ibraa1628@gmail.com>
2021-11-15 23:08:23 +0300

    ...

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const startMinus = 10;
 let time = startMinus * 60;
 
-const countdownEl = document.querySelector("#countdown");
+const countdownEl1 = document.querySelector(".countdown");
+const countdownEl2 = document.querySelector(".countdown2");
 
 setInterval(updateCountDown, 1000);
 
@@ -11,6 +12,7 @@ function updateCountDown() {
 
   seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  countdownEl.innerHTML = `${minutes}:${seconds}`;
+  countdownEl1.innerHTML = `${minutes}:${seconds}`;
+  countdownEl2.innerHTML = `${minutes}:${seconds}`;
   time--;
 }
